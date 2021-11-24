f83893433e18e2b31e4383d79d9d7333c5e3bbdb
emoranska <emilia.moranska@gmail.com>
2021-11-24 12:26:04 +0100

    add comments

--- vcf_open.py
+++ vcf_open.py
@@ -1,21 +1,22 @@
 from fuc import pyvcf
-import numpy as np
 
-import pandas as pd
+# open vcf file with fuc.pyvcf
+vf = pyvcf.VcfFrame.from_file('P1_no_Un_NW_MT.vcf')
 
-vf = pyvcf.VcfFrame.from_file('P1_short_test.vcf')
-
+# remove useless columns, leave GT
 just_gt = vf.strip('GT')
 
+# remove records with the same genotypes for all samples
 i = just_gt.df.apply(lambda r: len(set(r[9:])) != 1, axis=1)
-
 no_the_same_variant = just_gt.df[i]
 
+# create new VcfFrame
 to_vcf = pyvcf.VcfFrame([''], no_the_same_variant)
 
-P1_no_the_same_variant = to_vcf.to_file('P1_no_the_same_variant')
+# save VcfFrame to vcf file
+P1_no_the_same_variant = to_vcf.to_file('P1_no_the_same_variant.vcf')
 
-print(just_gt.df, no_the_same_variant, type(just_gt), type(no_the_same_variant))
+print(type(no_the_same_variant))
 
 
 
