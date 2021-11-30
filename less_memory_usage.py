dbc859c7231d90308f3ea0309d3309e4c5fd6f7a
emoranska <emilia.moranska@gmail.com>
2021-11-30 11:14:51 +0100

    Create functions: read_csv, concat_samples_gt, remove_the_same_gt, to_csv

--- less_memory_usage.py
+++ less_memory_usage.py
@@ -1,25 +1,41 @@
 import pandas as pd
 
 # reading csv files as pandas DataFrame
-data1 = pd.read_csv('P1_no_Un_NW_MT.vcf.default.csv', sep='\t')
-data2 = pd.read_csv('P1_no_Un_NW_MT.vcf.sample.GT.csv', sep='\t')
-#print(data1, data2)
+# data1 = pd.read_csv('P1_no_Un_NW_MT.vcf.default.csv', sep='\t')
+# data2 = pd.read_csv('P1_no_Un_NW_MT.vcf.sample.GT.csv', sep='\t')
+# print(data1, data2)
 
-# reset index in the second DataFrame (data2) which columns will be added to data1
-data2 = data2.reset_index(drop=True)
-#print(data2)
 
-# concatenate data1 and data2 - add data2 on the right of data1
-just_gt = pd.concat([data1, data2], axis=1)
+def read_csv(csv):
+    data = pd.read_csv(csv, sep='\t')
+    return data
+
+
+# reset index in the second DataFrame (data2_reset) which columns will be added to data1
+# concatenate data1 and data2_reset - add data2_reset on the right of data1
+def concat_samples_gt(csv_1, csv_2):
+    data1 = read_csv(csv_1)
+    data2_reset = read_csv(csv_2).reset_index(drop=True)
+    just_gt = pd.concat([data1, data2_reset], axis=1)
+    return just_gt
 
 # displaying result
-#print(just_gt.head())
+# print(just_gt.head())
+
 
 # remove records with the same genotypes for all samples
-i = just_gt.apply(lambda r: len(set(r[9:])) != 1, axis=1)
-no_the_same_variant = just_gt[i]
+def remove_the_same_gt(csv_1, csv_2):
+    data = concat_samples_gt(csv_1, csv_2)
+    i = data.apply(lambda r: len(set(r[9:])) != 1, axis=1)
+    no_the_same_variant = data[i]
+    return no_the_same_variant
+
 
 # save results to csv file
 # no_the_same_variant_csv = no_the_same_variant.to_csv('P1_no_the_same_variant.csv', index=False)
+def to_csv(pd_data, filename):
+    csv_out = pd_data.to_csv(filename, index=False)
+    return csv_out
 
-print(no_the_same_variant.head())
+
+# print(no_the_same_variant.head())
