93bcb6038e20ee91c3c40bbff05576f565bfb3a6
Emilia Moranska <emilia.moranska@gmail.com>
2022-07-01 16:44:07 +0200

    Clear script for finding homozygosity regions without break already in vcf file with fuc.pyvcf

--- finding_homozyg_vcf.py
+++ finding_homozyg_vcf.py
@@ -1,59 +1,36 @@
-import pandas as pd
-import csv
 from fuc import pyvcf
-import re
 import numpy as np
-from less_memory_usage import remove_the_same_gt, to_csv
-from vcf_open import remove_the_same_gt_vcf
 
-# dostosować regex do outputu z .vcf, popatrzeć też na wynik GT strip i ewentualnie zmienić drop.nan
-
-
-no_the_same_variant = remove_the_same_gt_vcf('P1_chr4_test.vcf')
+vf = pyvcf.VcfFrame.from_file('chr4_testdata1.vcf')
+just_gt = vf.strip('GT')
+i = just_gt.df.apply(lambda row: len(set(row[9:])) != 1, axis=1)
+no_the_same_variant = just_gt.df[i]
 
 no_the_same_variant.astype("string")
-
 
 # replace missing values signed as '.' with 'NaN'
 ntsv_nan = no_the_same_variant.replace('.', np.nan)
 
+print(ntsv_nan.to_string())
 
 # remove records with >=6 samples with missing values ('.' replaced with NaN)
-drop_dots = ntsv_nan.dropna(thresh=11, subset=['P1-25', 'P1-93', 'P1-88', 'P1-6', 'P1-89', 'P1-26', 'P1-12', 'P1-92',
-                                                'P1-22', 'P1-90', 'P1-28', 'P1-95'])
+drop_dots = ntsv_nan.dropna(thresh=7, subset=['P1-25', 'P1-93', 'P1-88', 'P1-6', 'P1-89', 'P1-26', 'P1-12', 'P1-92',
+                                              'P1-22', 'P1-90', 'P1-28', 'P1-95'])
 
-drop_dots_str = drop_dots.to_string()
-# print(type(drop_dots), type(drop_dots_str), drop_dots_str)
-print(drop_dots_str)
+# choosing rows that are >= 3 1/1 AND >=3 0/0 (solution from CAPSLOCK)
+cond_11x3_true = (drop_dots.iloc[:, 8:] == "1/1").sum(axis=1) >= 3
+cond_00x3_true = (drop_dots.iloc[:, 8:] == "0/0").sum(axis=1) >= 3
+result = drop_dots[cond_11x3_true & cond_00x3_true]
 
+print(result.to_string())
 
-# complete regex in methods.txt, here just part of it for testing
-# regex = r"^\d\s{3}chr\d?\s{2}\d+\s+\D+\d+\.\d+\s+\w+"
-regex = r"^\d\s{3}(chr\d)\s{2}\d+\s+\D+\d+\.\d+\s+\w+(?:(?:\s{2,}\d/\d)*?\s{2,}([10])/\2\b){3}.*"
-# regex = r"(?=(^\d\s{3}(chr\d)\s{2}\d+\s+\D+\d+\.\d+\s+\w+(?=(?:(?:\s+[01]/[01])*?\s+1/1){3})(?:(?:(?:\s+[01]/[01])*?\s+0/0){3})(?:\s+[01]/[01])*\s(^\d\s{3}((chr\d)\s{2}\d+\s+\D+\d+\.\d+\s+\w+(?:\s+[01]/[01])*\s)){1,2}^\d\s{3}(chr\d)\s{2}\d+\s+\D+\d+\.\d+\s+\w+(?=(?:(?:\s+[01]/[01])*?\s+1/1){3})(?:(?:(?:\s+[01]/[01])*?\s+0/0){3})(?:\s+[01]/[01])*\s))|(^\d\s{3}(chr\d)\s{2}\d+\s+\D+\d+\.\d+\s+\w+(?=(?:(?:\s+[01]/[01])*?\s+1/1){3})(?:(?:(?:\s+[01]/[01])*?\s+0/0){3})(?:\s+[01]/[01])*$)"
-matches = re.finditer(regex, drop_dots_str, re.MULTILINE)
+# replace missing values signed as 'NaN' with '.' for saving df to VcfFrame and vcf file
+final_df = result.replace(np.nan, '.')
 
-regions_homozyg = []
-for matchNum, match in enumerate(matches, start=1):
-    regions_homozyg.append(match.group())
-    # print(match.group())
-    # print("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum=matchNum, start=match.start(),
-    #                                                                     end=match.end(), match=match.group()))
+print(final_df.to_string())
 
-    # for groupNum in range(0, len(match.groups())):
-    #     groupNum = groupNum + 1
-    #
-    #     print("Group {groupNum} found at {start}-{end}: {group}".format(groupNum=groupNum, start=match.start(groupNum),
-    #                                                                     end=match.end(groupNum),
-    #                                                                     group=match.group(groupNum)))
-# print(type(matches), matches)
-print(type(regions_homozyg))
+# create new VcfFrame
+to_vcf = pyvcf.VcfFrame(['##fileformat=VCFv4.3'], final_df)
 
-
-# Open File
-resultFile = open("P1_chr4_output.csv", 'w')
-
-# Write data to file
-for r in regions_homozyg:
-    resultFile.write(r + "\n")
-resultFile.close()
+# save VcfFrame to vcf file
+chr4_test_homo_regions = to_vcf.to_file('1_chr4_test_homo_regions.vcf')
