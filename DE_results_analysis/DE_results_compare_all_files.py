4454a4c44ca7888a679d220d198ff3e333dfea1a
Emilia Moranska <emilia.moranska@gmail.com>
2023-08-02 17:55:43 +0200

    Change final df view, save it to csv and add lines for P4

--- DE_results_analysis/DE_results_compare_all_files.py
+++ DE_results_analysis/DE_results_compare_all_files.py
@@ -5,22 +5,25 @@ from pathlib import Path
 
 start_time = time.time()
 
-genes_in_bins = pd.read_csv('../homozyg_regions/P1_list_genes_in_homozyg_bins.csv', sep='\t')
+# genes_in_bins = pd.read_csv('../homozyg_regions/P1_list_genes_in_homozyg_bins.csv', sep='\t')
+genes_in_bins = pd.read_csv('../homozyg_regions/P4_list_genes_in_homozyg_bins.csv', sep='\t')
 genes_legend = pd.read_csv('genes.csv', sep=',').rename(columns={'Gene': 'GeneID'})
-list_to_de = pd.read_csv('../homozyg_regions/P1_list_to_DE_20more_genes.csv', sep='\t')
+
+# list_to_de = pd.read_csv('../homozyg_regions/P1_list_to_DE_20more_genes.csv', sep='\t')
+list_to_de = pd.read_csv('../homozyg_regions/P4_list_to_DE_20more_genes.csv', sep='\t')
 list_to_de['df_unique_index'] = list_to_de['df_unique_index'].apply(ast.literal_eval)
 
 final_df = pd.DataFrame()
-output_folder = Path("~/Pulpit/burak/DE_results_analysis/P1_DE_results")
 
-for idx in range(0, 2):
-    # file_name = f'P1_{idx}_de_result.csv'
+# input_folder = Path("~/Pulpit/Emi_P1")
+input_folder = Path("~/Pulpit/Emi_P4")
+
+for idx in range(0, 92):
+    # read the files with DE results according to path and current sample set number <idx> in loop
     file_name = f'one_one_zero_zero_{idx}_deseq2.xls'
-    file_path = output_folder / file_name
+    file_path = input_folder / file_name
     de_results = pd.read_table(file_path)
-    # de_results = pd.read_csv(file_path, sep='\t')
-    # de_results = pd.read_csv(f'P1_{idx}_de_result.csv', sep='\t')
-    print(de_results.to_string(max_rows=30))
+    # print(de_results.to_string(max_rows=30))
 
     # filter DE results for padj value < 0.05
     de_padj_filter = de_results[de_results['padj'] < 0.05].reset_index(drop=True)
@@ -32,7 +35,7 @@ for idx in range(0, 2):
 
     # filter list of genes in bins for appropriate sample set
     genes_in_bins_for_set = genes_in_bins[genes_in_bins['unique_no'].isin(unique_no)]
-    print(genes_in_bins_for_set.to_string(max_rows=30))
+    # print(genes_in_bins_for_set.to_string(max_rows=30))
 
     # merge filtered DE results with genes legend to identify genes names according to EL10 annotation
     de_padj_filter_merged = de_padj_filter.merge(genes_legend, on=['GeneID']).sort_values(by=['Chromosome', 'Start']). \
@@ -41,16 +44,19 @@ for idx in range(0, 2):
 
     # check if there are the same genes in bins for appropriate sample set as identified in DE analysis with padj < 0.05
     de_genes_in_bins = genes_in_bins_for_set.merge(de_padj_filter_merged, on=['gene_name']).drop(
-        columns=['genes_count'])
-    print(de_genes_in_bins.to_string())
+        columns=['genes_count', 'Chromosome', 'Strand'])
+    de_genes_in_bins['set_no'] = idx
+    de_genes_in_bins = de_genes_in_bins.reindex(columns=['set_no', 'unique_no', 'one_one', 'zero_zero', 'chr',
+                                                         'start_bin', 'end_bin', 'start_gene', 'end_gene',
+                                                         'gene_name', 'gene_strand', 'GeneID', 'Start', 'End',
+                                                         'baseMean', 'log2FoldChange', 'lfcSE', 'stat', 'pvalue',
+                                                         'padj', 'Biotype', 'Description'])
+    # print(de_genes_in_bins.to_string())
 
-    final_df = final_df.append(de_genes_in_bins)
+    final_df = final_df.append(de_genes_in_bins).reset_index(drop=True)
 
 print(final_df.to_string())
+# final_df.to_csv('P1_DE_results_comparing_with_genes_in_bins.csv', sep='\t', index=False)
+final_df.to_csv('P4_DE_results_comparing_with_genes_in_bins.csv', sep='\t', index=False)
 
 print("--- %s seconds ---" % (time.time() - start_time))
-
-
-
-
-
