41614116cce29b3e67c9dcf136c83731e1936355
Emilia Moranska <emilia.moranska@gmail.com>
2024-06-07 13:57:50 +0200

    Script for filter PopoolationTE2 output to create samples lists with and without TE insertions

---
+++ P1_P4/te_filter.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import time
+from pathlib import Path
+import ast
+
+start_time = time.time()
+
+te_matrix = pd.read_csv('../files/P1_matrix_all.csv', sep='\t')
+# print(te_matrix.to_string(max_rows=30))
+
+cols = te_matrix.filter(like='P1-').columns
+sep = ','
+
+# create column with list of samples with MITE insertions, interpreted as frequency > 0.8
+te_matrix['ok_ins'] = (pd.Series(te_matrix[cols].ge(0.8).dot((cols + sep))).str.rstrip(sep).str.split(',').
+                       sort_values().apply(lambda x: sorted(x)))
+
+# create column with list of samples with frequency < 0.3
+te_matrix['less_0.3'] = (pd.Series(te_matrix[cols].le(0.3).dot((cols + sep))).str.rstrip(sep).str.split(',').
+                         sort_values().apply(lambda x: sorted(x)))
+
+# create column with list of samples with frequency == 0
+te_matrix['eq_0'] = (pd.Series(te_matrix[cols].eq(0).dot((cols + sep))).str.rstrip(sep).str.split(',').sort_values().
+                     apply(lambda x: sorted(x)))
+
+# create column with list of samples without MITE insertions --> frequency < 0.3 and != 0
+notna_only = te_matrix[['family']].notna().all(axis=1)
+te_matrix.loc[notna_only, 'no_ins'] = [list(sorted(set(a) - set(b))) for a, b in
+                                       zip(te_matrix.loc[notna_only, 'less_0.3'], te_matrix.loc[notna_only, 'eq_0'])]
+
+# create columns with list of 3 first samples with and without insertions
+te_matrix['ins_3first'] = te_matrix['ok_ins'].apply(lambda y: y[0:3])
+te_matrix['no_ins_3first'] = te_matrix['no_ins'].apply(lambda i: i[0:3])
+
+print(te_matrix.to_string(max_rows=30))
+
+te_filter = te_matrix.drop(columns=['less_0.3', 'eq_0', 'no_ins', 'ok_ins']).rename(columns={
+    'ins_3first': 'te_ins', 'no_ins_3first': 'no_te_ins'})
+
+print(te_filter.to_string(max_rows=50))
+
+'''
+# checking data type in new columns because of problems with empty strings inside lists
+print(te_filter.loc[16, 'no_te_ins'], type(te_filter.loc[16, 'no_te_ins']), len(te_filter.loc[16, 'no_te_ins']))
+for x in te_filter.loc[16, 'no_te_ins']:
+    print(x, type(x), len(x))
+
+print(te_filter.loc[13, 'no_te_ins'], type(te_filter.loc[13, 'no_te_ins']), len(te_filter.loc[13, 'no_te_ins']))
+for x in te_filter.loc[13, 'no_te_ins']:
+    print(x, type(x), len(x), x.count(''))
+
+print(te_filter.loc[1, 'te_ins'], type(te_filter.loc[1, 'te_ins']), len(te_filter.loc[1, 'te_ins']))
+for x in te_filter.loc[1, 'te_ins']:
+    print(x, type(x), len(x), x.count(''))
+
+print(te_filter.loc[13, 'te_ins'], type(te_filter.loc[13, 'te_ins']), len(te_filter.loc[13, 'te_ins']))
+for x in te_filter.loc[13, 'te_ins']:
+    print(x, len(x), x.count(''))
+    print(type(x))
+'''
+
+# filter out empty strings from list of samples in 'te_ins' columns !!!
+te_filter['te_ins'] = te_filter['te_ins'].apply(lambda x: list(filter(None, x)))
+
+# checking previous line effect
+for x in te_filter.loc[13, 'te_ins']:
+    print(x, len(x), x.count(''))
+    print(type(x))
+
+# create final list of <=3 samples with and without MITE insertions
+te_filter['no_te_ins'] = te_filter['no_te_ins'].astype(str)
+te_filter['te_ins'] = te_filter['te_ins'].astype(str)
+te_filter_final = (te_filter.loc[(te_filter['te_ins'] != '[]') & (te_filter['no_te_ins'] != '[]')])
+print(te_filter_final.to_string(max_rows=30))
+
+te_filter_final = te_filter_final.drop(columns=te_filter_final.filter(like='P1-').columns).reset_index(drop=True)
+print(te_filter_final.to_string(max_rows=30))
+
+te_filter_final.to_csv('../files/P1_MITE_list.csv', sep='\t', index=False)
+
+print("--- %s seconds ---" % (time.time() - start_time))
