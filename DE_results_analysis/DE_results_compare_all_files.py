7594e97ca07694bb8a7c3e1c6b2b8fe34d108e66
Emilia Moranska <emilia.moranska@gmail.com>
2024-07-19 14:45:26 +0200

    Add lines for counting sum of genes identified in DE for every sample set

--- DE_results_analysis/DE_results_compare_all_files.py
+++ DE_results_analysis/DE_results_compare_all_files.py
@@ -5,38 +5,40 @@ from pathlib import Path
 
 start_time = time.time()
 
-# genes_in_bins = pd.read_csv('../homozyg_regions/P1_list_genes_in_homozyg_bins.csv', sep='\t')
-# genes_in_bins = pd.read_csv('../homozyg_regions/P4_list_genes_in_homozyg_bins.csv', sep='\t')
-genes_in_bins = pd.read_csv('../files/P1_list_genes_in_rest_bins_to_add.csv', sep='\t')
+# genes_in_bins = pd.read_csv('../files/P1_list_genes_in_rest_bins_to_add.csv', sep='\t')
+genes_in_bins = pd.read_csv('../files/P4_list_genes_in_rest_bins_to_add.csv', sep='\t')
 
-# genes_legend = pd.read_csv('genes.csv', sep=',').rename(columns={'Gene': 'GeneID'})
 genes_legend = pd.read_csv('../../../Pobrane/genes.csv', sep=',').rename(columns={'Gene': 'GeneID'})
 
-# list_to_de = pd.read_csv('../homozyg_regions/P1_list_to_DE_20more_genes.csv', sep='\t')
-# list_to_de = pd.read_csv('../homozyg_regions/P4_list_to_DE_20more_genes.csv', sep='\t')
-list_to_de = pd.read_csv('../../../Pobrane/P1_list_to_DE_20more_genes.csv', sep='\t')
+# list_to_de = pd.read_csv('../../../Pobrane/P1_list_to_DE_20more_genes.csv', sep='\t')
+list_to_de = pd.read_csv('../../../Pobrane/P4_list_to_DE_20more_genes.csv', sep='\t')
 
 list_to_de['df_unique_index'] = list_to_de['df_unique_index'].apply(ast.literal_eval)
 
-# input_folder = Path("~/Pulpit/Emi_P1")
-# input_folder = Path("~/Pulpit/Emi_P4")
-input_folder = Path("~/Pobrane/Emi_P1")
+# input_folder = Path("~/Pobrane/Emi_P1")
+input_folder = Path("~/Pobrane/Emi_P4")
 
 
 def de_results_with_genes_in_bins():
     final_df = pd.DataFrame()
 
     # set the range according to numer of output files with DE results
-    for idx in range(0, 97):
+    for idx in range(0, 92):
         # read the files with DE results according to path and current sample set number <idx> in loop
         file_name = f'one_one_zero_zero_{idx}_deseq2.xls'
         file_path = input_folder / file_name
         de_results = pd.read_table(file_path)
         # print(de_results.to_string(max_rows=30))
 
+        # print list with sum of all genes identified in DE for every sample set
+        # print(len(de_results))
+
         # filter DE results for padj value < 0.05
         de_padj_filter = de_results[de_results['padj'] < 0.05].reset_index(drop=True)
         # print(de_padj_filter.to_string(max_rows=30))
+
+        # print list with sum of genes with padj < 0.05 identified in DE for every sample set
+        print(len(de_padj_filter))
 
         # merge filtered DE results with genes legend to identify genes names according to EL10 annotation
         de_padj_filter_merged = (de_padj_filter.merge(genes_legend, on=['GeneID']).sort_values
@@ -72,13 +74,13 @@ def de_results_with_genes_in_bins():
     print(final_df.to_string(max_rows=50))
     # final_df.to_csv('P1_DE_results_comparing_with_genes_in_bins.csv', sep='\t', index=False)
     # final_df.to_csv('P4_DE_results_comparing_with_genes_in_bins.csv', sep='\t', index=False)
-    final_df.to_csv('P1_DE_results_comparing_with_genes_in_rest_bins.csv', sep='\t', index=False)
+    # final_df.to_csv('P1_DE_results_comparing_with_genes_in_rest_bins.csv', sep='\t', index=False)
 
 def xloc_checking():
     final_df = pd.DataFrame()
 
     # set the range according to numer of output files with DE results
-    for idx in range(0, 97):
+    for idx in range(0, 92):
         # read the files with DE results according to path and current sample set number <idx> in loop
         file_name = f'one_one_zero_zero_{idx}_deseq2.xls'
         file_path = input_folder / file_name
@@ -87,6 +89,7 @@ def xloc_checking():
         # filter DE results for GeneID = XLOC
         de_padj_filter = de_results[de_results.GeneID.str.startswith('XLOC')].reset_index(drop=True)
         # print(de_padj_filter.to_string(max_rows=30))
+        print(len(de_padj_filter))
 
         # merge filtered DE results with genes legend to add columns with coordinates for XLOC genes
         de_padj_filter_merged = de_padj_filter.merge(genes_legend, on=['GeneID']).sort_values(
@@ -118,13 +121,13 @@ def xloc_checking():
         # final_df = final_df.append(de_xloc_genes_in_bins).reset_index(drop=True)
         final_df = pd.concat([final_df, de_xloc_genes_in_bins], ignore_index=True)
 
-        final_df.to_csv('P1_XLOC_comparing_with_rest_bins_with_genes.csv', sep='\t', index=False)
+        # final_df.to_csv('P1_XLOC_comparing_with_rest_bins_with_genes.csv', sep='\t', index=False)
         # final_df.to_csv('P4_XLOC_comparing_with_bins_with_genes.csv', sep='\t', index=False)
 
     print(final_df.to_string())
 
 
-# de_results_with_genes_in_bins()
-xloc_checking()
+de_results_with_genes_in_bins()
+# xloc_checking()
 
 print("--- %s seconds ---" % (time.time() - start_time))
