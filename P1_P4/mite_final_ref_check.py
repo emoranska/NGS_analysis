de901a040c8d95cbb6defd7b007560c722aa5e73
Emilia Moranska <emilia.moranska@gmail.com>
2024-08-05 12:58:11 +0200

    Script for calculation reference and non-reference MITEs in result files

--- P1_P4/mite_final_ref_check.py
+++ P1_P4/mite_final_ref_check.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-te_final = pd.read_csv('../files/P4_MITEs_DE_bins.csv', sep='\t')
-te_ref = pd.read_csv('../files/P4_ref_matrix_sort_2000_all.csv', sep='\t')
-te_nonref = pd.read_csv('../files/P4_nonref_matrix_all.csv', sep='\t')
+te_final = pd.read_csv('../files/P1_MITEs_updown2000_DE_bins.csv', sep='\t')
+te_ref = pd.read_csv('../files/P1_ref_matrix_sort_2000_all.csv', sep='\t')
+te_nonref = pd.read_csv('../files/P1_nonref_matrix_all.csv', sep='\t')
 
 ref_check = te_final.merge(te_ref, on=['chr', 'family', 'start_te', 'end_te'])
-print(f'Reference MITES: {ref_check}')
+print(f'Reference MITEs: \n {ref_check}')
 
 nonref_check = te_final.merge(te_nonref, on=['chr', 'family', 'start_te', 'end_te'])
-print(f'Non-reference MITES: {nonref_check}')
+print(f'Non-reference MITEs: \n {nonref_check}')
