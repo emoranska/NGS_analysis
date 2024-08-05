b77eea8537177c3d8edbae4d21ecbdce178c1537
Emilia Moranska <emilia.moranska@gmail.com>
2024-08-05 12:47:35 +0200

    Script for concatenating two parts of results for P4 and MITEs in annotated genes and XLOCs

--- P1_P4/concat_P4.py
+++ P1_P4/concat_P4.py
@@ -18,3 +18,26 @@ unique_bins_count_m = m_all.drop_duplicates(subset=['start_bin', 'end_bin'])
 print(f'Number of bins with MITEs in DE genes: {len(unique_bins_count_m)}')
 
 print(m_all.to_string(max_rows=30))
+
+ud1 = pd.read_csv('../files/P4_correct_MITEs_updown2000_DE_bins_1.csv', sep='\t')
+ud2 = pd.read_csv('../files/P4_correct_MITEs_updown2000_DE_bins_2.csv', sep='\t')
+
+ud_all = pd.concat([ud1, ud2], ignore_index=True)
+ud_all.to_csv('../files/P4_correct_MITEs_updown2000_DE_bins_all.csv', sep='\t', index=False)
+
+unique_bins_count_ud = ud_all.drop_duplicates(subset=['start_bin', 'end_bin'])
+print(f'Number of bins with MITEs in DE genes: {len(unique_bins_count_ud)}')
+
+print(ud_all.to_string(max_rows=30))
+
+xloc = pd.read_csv('../files/P4_correct_MITEs_XLOC_DE_bins.csv', sep='\t')
+
+m_all_and_xloc = pd.concat([m_all, xloc], ignore_index=True).sort_values(by=['chromosome', 'start_te']).fillna('-')
+print(m_all_and_xloc.to_string(max_rows=80))
+m_all_and_xloc.to_csv('../files/P4_correct_MITEs_DE_bins_all_and_XLOC.csv', sep='\t', index=False)
+
+xloc_ud = pd.read_csv('../files/P4_correct_MITEs_updown2000_XLOC_DE_bins.csv', sep='\t')
+ud_all_and_xloc = (pd.concat([ud_all, xloc_ud], ignore_index=True).sort_values(by=['chromosome', 'start_te']).
+                   fillna('-'))
+print(ud_all_and_xloc.to_string(max_rows=50))
+ud_all_and_xloc.to_csv('../files/P4_correct_MITEs_updown2000_DE_bins_all_and_XLOC.csv', sep='\t', index=False)
