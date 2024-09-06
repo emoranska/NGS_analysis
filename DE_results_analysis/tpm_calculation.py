06166d33e988abdf430d6fa46295b54833fb9fee
Emilia Moranska <emilia.moranska@gmail.com>
2024-09-06 13:09:26 +0200

    Tpm calculation

--- DE_results_analysis/tpm_calculation.py
+++ DE_results_analysis/tpm_calculation.py
@@ -11,12 +11,23 @@ print(genes_and_raw.to_string(max_rows=30))
 
 # calculate RPK values
 cols = genes_and_raw.filter(like='P1-').columns
+
 genes_and_raw[cols] = genes_and_raw[cols].div(genes_and_raw['gene_length_kb'], axis=0)
 print(genes_and_raw.to_string(max_rows=30))
+
+rpk = genes_and_raw[cols]
 
 # calculate scaling factor
 scaling_factor = genes_and_raw[cols].sum() / 1000000
 scaling_factor = scaling_factor.reset_index()
-scaling_factor.columns = ['sample', 'scaling_factor']
+scaling_factor.columns = ['sample', 'factor']
 print(scaling_factor.to_string(max_rows=30))
 
+tpm = rpk / scaling_factor.set_index('sample')['factor']
+tpm['GeneID'] = genes_and_raw['GeneID']
+print(tpm)
+
+tpm_sum = tpm.sum()
+print(tpm_sum)
+
+tpm.to_csv('../files/P1_kallisto_tpm.csv', sep='\t', index=False)
