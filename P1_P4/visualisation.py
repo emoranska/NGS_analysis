4036b012748605d6bfdfbb72c172bc40c9df4587
Emilia Moranska <emilia.moranska@gmail.com>
2024-10-02 15:56:56 +0200

    Add args

--- P1_P4/visualisation.py
+++ P1_P4/visualisation.py
@@ -3,16 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# superfam_de_005 = (pd.read_csv('../files/P4_DE_005_superfam_counts.csv', sep='\t',
-#                                dtype={'MITE_sum': np.int32, 'upstream': np.int32, 'downstream': np.int32,
-#                                       'intron': np.int32, 'cds': np.int32}).set_index('superfamily'))
-#
-# superfam_de_005_data = superfam_de_005[['upstream', 'downstream', 'intron', 'cds']]
-# print(superfam_de_005_data.to_string())
-#
-# superfam_all_genes = (pd.read_csv('../files/P4_all_genes_superfam_counts.csv', sep='\t').
-#                       set_index('superfamily'))
-# superfam_all_genes_data = superfam_all_genes[['upstream', 'downstream', 'intron', 'cds']]
+superfam_de_005 = (pd.read_csv('../files/P4_DE_005_superfam_counts.csv', sep='\t').
+                   set_index('superfamily'))
+
+superfam_all_genes = (pd.read_csv('../files/P4_all_genes_superfam_counts.csv', sep='\t').
+                      set_index('superfamily'))
 
 families_de_005 = pd.read_csv('../files/P4_families_DE_005.csv', sep='\t').set_index('family')
 families_all_genes = pd.read_csv('../files/P4_families_all_genes.csv', sep='\t').set_index('family')
@@ -27,7 +22,7 @@ def heatmap(data, title, ylabel_fontsize, values_fontsize, cbar_param):
                      cbar_kws=cbar_param)
     plt.title(title, fontsize=20)
     plt.xlabel('Localisation', fontsize=12)
-    plt.ylabel('Family', fontsize=12)
+    plt.ylabel('Superfamily', fontsize=12)
 
     cbar = hm.collections[0].colorbar
     cbar.ax.tick_params(labelsize=9)
@@ -36,9 +31,9 @@ def heatmap(data, title, ylabel_fontsize, values_fontsize, cbar_param):
     plt.show()
 
 
-# heatmap(superfam_de_005_data, 'P4 MITEs (for DE genes < 0.05)')
-# heatmap(superfam_all_genes_data, 'P4 MITEs (for all genes)')
+# heatmap(superfam_de_005[['upstream', 'downstream', 'intron', 'cds']], 'P4 MITEs (for DE genes < 0.05)', 10, 10, {"pad": 0.02, "shrink": 0.5})
+heatmap(superfam_all_genes[['upstream', 'downstream', 'intron', 'cds']], 'P4 MITEs (for all genes)', 10, 10, {"pad": 0.02, "shrink": 0.5})
 # heatmap(families_de_005[['upstream', 'downstream', 'intron', 'cds']], 'P4 MITEs (for DE genes < 0.05)',
 #     7, 7, {"pad": 0.01, "shrink": 0.35})
-heatmap(families_all_genes[['upstream', 'downstream', 'intron', 'cds']], 'P4 MITEs (for all genes)',
-        5,5, {"pad": 0.01, "shrink": 0.35})
+# heatmap(families_all_genes[['upstream', 'downstream', 'intron', 'cds']], 'P4 MITEs (for all genes)',
+#         5,5, {"pad": 0.01, "shrink": 0.35})
