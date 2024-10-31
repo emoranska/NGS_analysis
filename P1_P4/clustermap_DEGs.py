e094f6ffd58c5a0a5b1749abb1f5d0f1e62a18ec
Emilia Moranska <emilia.moranska@gmail.com>
2024-10-31 13:29:57 +0100

    Additional legend for MITE localisation

--- P1_P4/clustermap_DEGs.py
+++ P1_P4/clustermap_DEGs.py
@@ -4,10 +4,34 @@ import seaborn as sns
 
 data_stow_3 = pd.read_csv('../files/Stow3_clustermap_data.csv', sep='\t', header=0, index_col=0)
 
-genes = data_stow_3.index
-sample_names = data_stow_3.columns
 
-degs_clustermap = sns.clustermap(data_stow_3, xticklabels=sample_names, yticklabels=genes, z_score=0, col_cluster=False,
-                                 figsize=(7,9))
+def clustermap_degs(data, mite_fam):
+    genes = data.index
+    sample_names = data.columns
 
-plt.show()
+    d_st3 = data[data.columns[0:6]]
+
+    # to check how to add color labels to rows according to values from 'mite_loc'
+    mite_loc_labels = data['mite_loc']
+    mite_loc_pal = sns.color_palette("Spectral_r", 3)
+    mite_loc_lut = dict(zip(mite_loc_labels.unique(), mite_loc_pal))
+    row_colors = mite_loc_labels.map(mite_loc_lut)
+
+    degs_clustermap = sns.clustermap(d_st3, cmap="bwr", xticklabels=sample_names, yticklabels=genes, z_score=0, center=0,
+                                     col_cluster=False, figsize=(8,9), row_colors=row_colors, cbar_kws={'label': 'row Z-score'},
+                                     cbar_pos=(.99, .65, .02, .15))
+
+    ax = degs_clustermap.ax_heatmap
+    ax.set_ylabel("")
+
+    ax.set_title(f"{mite_fam} MITEs associated with DEGs", fontsize=20)
+
+    for label in mite_loc_labels.unique():
+        degs_clustermap.ax_col_dendrogram.bar(0, 0, color=mite_loc_lut[label],
+                                label=label, linewidth=0)
+    degs_clustermap.ax_col_dendrogram.legend(loc="center", ncol=4)
+
+    degs_clustermap.savefig(f'../files/clustermaps/{mite_fam}.pdf')
+    plt.show()
+
+clustermap_degs(data_stow_3, "Stow3")
