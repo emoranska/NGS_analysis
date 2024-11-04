b6b34c1b2ba05903a21c391082ad8180bf69b708
Emilia Moranska <emilia.moranska@gmail.com>
2024-11-04 13:19:23 +0100

    Add coloured y-labels depending on MITEs effect on DE regulation

--- P1_P4/clustermap_DEGs.py
+++ P1_P4/clustermap_DEGs.py
@@ -2,14 +2,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data_stow_3 = pd.read_csv('../files/Stow3_clustermap_data.csv', sep='\t', header=0, index_col=0)
+
+data_stow_3 = pd.read_csv('../files/heatmaps/cm_data/Stow3_clustermap_data.csv', sep='\t',
+                          header=0, index_col=0)
 
 
 def clustermap_degs(data, mite_fam):
     genes = data.index
     sample_names = data.columns
 
-    d_st3 = data[data.columns[0:6]]
+    data_tpm = data[data.columns[0:6]]
 
     # to check how to add color labels to rows according to values from 'mite_loc'
     mite_loc_labels = data['mite_loc']
@@ -17,14 +19,16 @@ def clustermap_degs(data, mite_fam):
     mite_loc_lut = dict(zip(mite_loc_labels.unique(), mite_loc_pal))
     row_colors = mite_loc_labels.map(mite_loc_lut)
 
-    degs_clustermap = sns.clustermap(d_st3, cmap="bwr", xticklabels=sample_names, yticklabels=genes, z_score=0, center=0,
-                                     col_cluster=False, figsize=(8,9), row_colors=row_colors, cbar_kws={'label': 'row Z-score'},
-                                     cbar_pos=(.99, .65, .02, .15))
+    degs_clustermap = sns.clustermap(data_tpm, cmap="bwr", xticklabels=sample_names, yticklabels=genes, z_score=0,
+                                     center=0, col_cluster=False, figsize=(6, 7), row_colors=row_colors,
+                                     cbar_kws={'label': 'row Z-score'}, cbar_pos=(.95, .65, .02, .15))
 
     ax = degs_clustermap.ax_heatmap
     ax.set_ylabel("")
 
-    ax.set_title(f"{mite_fam} MITEs associated with DEGs", fontsize=20)
+    ax.set_title(f"{mite_fam} MITEs associated with DEGs", fontsize=18)
+    degs_clustermap.tick_params(axis='x', labelsize=8)
+    degs_clustermap.tick_params(axis='y', labelsize=8)
 
     for label in mite_loc_labels.unique():
         degs_clustermap.ax_col_dendrogram.bar(0, 0, color=mite_loc_lut[label],
@@ -34,14 +38,19 @@ def clustermap_degs(data, mite_fam):
 
 # check for changing y label colours
     genes_labels = data['ins_de']
-    genes_lut = dict(zip(genes_labels.unique(), 'bwr'))
+    print(type(genes_labels), genes_labels)
+    genes_lut = dict(zip(genes_labels.unique(), 'rbg'))
+    print(type(genes_lut), print(genes_lut))
 
     for tick_label in degs_clustermap.ax_heatmap.axes.get_yticklabels():
         tick_text = tick_label.get_text()
+        print(type(tick_text), tick_text)
         gene_name = genes_labels.loc[tick_text]
+        print(type(gene_name), gene_name)
         tick_label.set_color(genes_lut[gene_name])
 
-    # degs_clustermap.savefig(f'../files/clustermaps/{mite_fam}.pdf')
+    degs_clustermap.savefig(f'../files/heatmaps/clustermaps/{mite_fam}.pdf')
     plt.show()
+
 
 clustermap_degs(data_stow_3, "Stow3")
