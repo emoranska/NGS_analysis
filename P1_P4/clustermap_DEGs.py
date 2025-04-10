d9a8b9ab0b63bed65ac373721ba77c4f25498892
Emilia Moranska <emilia.moranska@gmail.com>
2025-04-10 16:51:47 +0200

    Add lines to construct clustermap for all DEGs

--- P1_P4/clustermap_DEGs.py
+++ P1_P4/clustermap_DEGs.py
@@ -2,22 +2,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data_stow_3 = pd.read_csv('../files/heatmaps/cm_data/Stow3_cm_data_correct.csv', sep='\t',
-                          header=0, index_col=0)
-data_stow_36 = pd.read_csv('../files/heatmaps/cm_data/Stow36_cm_data_correct.csv', sep='\t', header=0,
-                           index_col=0)
-data_stow_40 = pd.read_csv('../files/heatmaps/cm_data/Stow40_cm_data_correct.csv', sep='\t', header=0,
-                           index_col=0)
-data_tourist_1 = pd.read_csv('../files/heatmaps/cm_data/Tourist1_cm_data_correct.csv', sep='\t', header=0,
-                             index_col=0)
-data_tourist_17 = pd.read_csv('../files/heatmaps/cm_data/Tourist17_cm_data_correct.csv', sep='\t', header=0,
-                              index_col=0)
-data_uc_33 = pd.read_csv('../files/heatmaps/cm_data/uc33_cm_data_correct.csv', sep='\t', header=0,
-                         index_col=0)
-data_deg_with_mites = pd.read_csv('../files/heatmaps/cm_data/P1_DE005_with_MITEs_cm_general_data_final_1.csv',
-                                  sep='\t', header=0, index_col=0)
-data_all_genes_with_mites = pd.read_csv('../files/heatmaps/cm_data/'
-                                        'P1_all_genes_with_MITEs_cm_general_data_final_1.csv',
+# data_stow_3 = pd.read_csv('../files/heatmaps/cm_data/Stow3_cm_data_correct.csv', sep='\t',
+#                           header=0, index_col=0)
+# data_stow_36 = pd.read_csv('../files/heatmaps/cm_data/Stow36_cm_data_correct.csv', sep='\t', header=0,
+#                            index_col=0)
+# data_stow_40 = pd.read_csv('../files/heatmaps/cm_data/Stow40_cm_data_correct.csv', sep='\t', header=0,
+#                            index_col=0)
+# data_tourist_1 = pd.read_csv('../files/heatmaps/cm_data/Tourist1_cm_data_correct.csv', sep='\t', header=0,
+#                              index_col=0)
+# data_tourist_17 = pd.read_csv('../files/heatmaps/cm_data/Tourist17_cm_data_correct.csv', sep='\t', header=0,
+#                               index_col=0)
+# data_uc_33 = pd.read_csv('../files/heatmaps/cm_data/uc33_cm_data_correct.csv', sep='\t', header=0,
+#                          index_col=0)
+# data_deg_with_mites = pd.read_csv('../files/heatmaps/cm_data/P1_DE005_with_MITEs_cm_general_data_final_1.csv',
+#                                   sep='\t', header=0, index_col=0)
+# data_all_genes_with_mites = pd.read_csv('../files/heatmaps/cm_data/'
+#                                         'P1_all_genes_with_MITEs_cm_general_data_final_1.csv',
+#                                         sep='\t', header=0, index_col=0)
+# data_all_degs = pd.read_csv('../files/clustermaps/P1_all_DEGs_cm_general_data_final.csv',
+#                                         sep='\t', header=0, index_col=0)
+data_all_degs = pd.read_csv('../files/clustermaps/P1_DE005_with_MITEs_cm_general_data_final_1.csv',
                                         sep='\t', header=0, index_col=0)
 
 
@@ -30,6 +34,7 @@ def clustermap_degs(data, mite_fam):
     # variables for adding color labels to rows according to values from 'mite_loc'
     mite_loc_labels = data['mite_loc']
     mite_loc_options = ['upstream', '5UTR', 'intron', 'cds', '3UTR', 'downstream']
+    # mite_loc_options = ['upstream', '5UTR', 'intron', 'cds', '3UTR', 'downstream', 'no_TE']
     mite_loc_pal = sns.color_palette("Spectral_r", 7)
     mite_loc_lut = dict(zip(mite_loc_options, mite_loc_pal))
     print(mite_loc_lut)
@@ -47,17 +52,21 @@ def clustermap_degs(data, mite_fam):
 
     # set main title and axis labels font size
     # ax.set_title(f"{mite_fam} MITEs associated with DEGs", fontsize=18)
-    ax.set_title(f"{mite_fam} MITEs associated with all genes", fontsize=18)
+    # ax.set_title(f"{mite_fam} MITEs associated with all genes", fontsize=18)
+    ax.set_title(f"{mite_fam} DEGs", fontsize=18)
     degs_clustermap.tick_params(axis='x', labelsize=8)
     degs_clustermap.tick_params(axis='y', labelsize=4)
+    # plt.setp(degs_clustermap.ax_heatmap.get_xticklabels(), labelsize=8)
+    # plt.setp(degs_clustermap.ax_heatmap.get_yticklabels(), labelsize=4)
 
     # add color labels to rows according to values from 'mite_loc'
     for label in mite_loc_labels.unique():
         degs_clustermap.ax_col_dendrogram.bar(0, 0, color=mite_loc_lut[label], label=label, linewidth=0)
     degs_clustermap.ax_col_dendrogram.legend(loc="center", ncol=4)
 
-    # changing y-axis label colours according to 'ins_de' values
+    # changing y-axis label colours according to 'ins_de' or 'de_all' values
     genes_labels = data['ins_de']
+    # genes_labels = data['de_all']
     print(type(genes_labels), genes_labels)
     # genes_labels_options = ['ins_up', 'ins_down']
     genes_labels_options = ['up', 'down']
@@ -73,7 +82,8 @@ def clustermap_degs(data, mite_fam):
         print(type(gene_name), gene_name)
         tick_label.set_color(genes_lut[gene_name])
 
-    degs_clustermap.savefig(f'../files/heatmaps/clustermaps/{mite_fam}_MITES_DEGs_1.pdf')
+    # degs_clustermap.savefig(f'../files/heatmaps/clustermaps/{mite_fam}_DEGs.pdf')
+    # degs_clustermap.savefig(f'../files/clustermaps/{mite_fam}_DEGs.pdf')
     plt.show()
 
 
@@ -83,5 +93,7 @@ def clustermap_degs(data, mite_fam):
 # clustermap_degs(data_tourist_1, "Tourist1")
 # clustermap_degs(data_tourist_17, "Tourist17")
 # clustermap_degs(data_uc_33, "uc33")
-clustermap_degs(data_deg_with_mites, ' P1_all')
+# clustermap_degs(data_deg_with_mites, ' P1_all')
 # clustermap_degs(data_all_genes_with_mites, ' P1_all')
+clustermap_degs(data_all_degs, 'P1_all')
+
--- P1_P4/clustermap_data_transform.py
+++ P1_P4/clustermap_data_transform.py
@@ -2,8 +2,9 @@ import pandas as pd
 import ast
 import numpy as np
 
-df_matrix = pd.read_csv('../files/heatmaps/cm_data/P1_DE005_with_MITEs_cm_general_data.csv', sep='\t')
+# df_matrix = pd.read_csv('../files/heatmaps/cm_data/P1_DE005_with_MITEs_cm_general_data.csv', sep='\t')
 # df_matrix = pd.read_csv('../files/heatmaps/cm_data/P1_all_genes_with_MITEs_cm_general_data_1.csv', sep='\t')
+df_matrix = pd.read_csv('../files/clustermaps/P1_all_DEGs_cm_general_data.csv', sep='\t')
 
 print(df_matrix.to_string(max_rows=30))
 
@@ -17,7 +18,7 @@ df_matrix_trans = df_matrix.assign(**{f"{k}_{i+1}": df_matrix.values[np.arange(
 
 print(df_matrix_trans.to_string(max_rows=30))
 
-m = df_matrix_trans['ins_de'] != df_matrix_trans['de_results']
+m = (df_matrix_trans['ins_de'] != df_matrix_trans['de_results']) & (df_matrix_trans['ins_de'] != 'no_TE')
 df_matrix_trans.loc[m, ['one_one_1', 'zero_zero_1']] = df_matrix_trans.loc[
     m, ['zero_zero_1', 'one_one_1']].values  # Swap rows if condition met
 
@@ -34,11 +35,15 @@ df_final = df_matrix_trans.rename(columns={'one_one_1': 'ins_1', 'one_one_2': 'i
                                            'zero_zero_3': 'no_ins_3'})
 #            drop(columns=df_matrix_trans.loc[:, 'one_one':'no_te_ins']))
 #             drop(columns=df_matrix_trans.loc[:, 'ins_up_1': 'ins_down'])).drop(columns=['de_results', 'ins_check'])
+# df_final = df_final.loc[:, ['gene_name', 'ins_1', 'ins_2', 'ins_3', 'no_ins_1', 'no_ins_2', 'no_ins_3', 'mite_loc',
+#                             'ins_de', 'family']]
 df_final = df_final.loc[:, ['gene_name', 'ins_1', 'ins_2', 'ins_3', 'no_ins_1', 'no_ins_2', 'no_ins_3', 'mite_loc',
-                            'ins_de', 'family']]
+                            'de_all', 'family']]
 print(df_final.to_string(max_rows=30))
 
-df_final = (df_final.drop_duplicates().drop_duplicates(subset=['gene_name', 'mite_loc', 'ins_de', 'family']).
+# df_final = (df_final.drop_duplicates().drop_duplicates(subset=['gene_name', 'mite_loc', 'ins_de', 'family']).
+#             reset_index(drop=True))
+df_final = (df_final.drop_duplicates().drop_duplicates(subset=['gene_name', 'mite_loc', 'de_all', 'family']).
             reset_index(drop=True))
 print(df_final.to_string(max_rows=30))
 
@@ -53,5 +58,6 @@ m = g.transform('size').gt(1)
 # increment values
 df_final.loc[m, 'gene_name'] += '_TE'+g.cumcount().add(1).astype(str)
 print(df_final.to_string(max_rows=30))
-df_final.to_csv('../files/heatmaps/cm_data/P1_DE005_with_MITEs_cm_general_data_final_1.csv', sep='\t', index=False)
+# df_final.to_csv('../files/heatmaps/cm_data/P1_DE005_with_MITEs_cm_general_data_final_1.csv', sep='\t', index=False)
 # df_final.to_csv('../files/heatmaps/cm_data/P1_all_genes_with_MITEs_cm_general_data_final_1.csv', sep='\t', index=False)
+df_final.to_csv('../files/clustermaps/P1_all_DEGs_cm_general_data_final.csv', sep='\t', index=False)
