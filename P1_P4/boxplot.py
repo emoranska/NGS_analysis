90d15213fc3ec6f5e40a663802f88f1a5beae52c
Emilia Moranska <emilia.moranska@gmail.com>
2025-05-16 13:05:21 +0200

    Cosmetic

--- P1_P4/boxplot.py
+++ P1_P4/boxplot.py
@@ -3,17 +3,21 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 superfam_all_mites = pd.read_csv('../files/boxplots/P1_boxplot_data.csv', sep='\t')
+superfam_de005 = pd.read_csv('../files/boxplots/P4_DE005_boxplot_data.csv', sep='\t')
 
 
 def boxplot(data):
     plt.figure(figsize=(10, 10))
     bplot = sns.boxplot(data, x="MITE_group", y="MITE_sum")
+    bplot.set_yticks(range(len(data)-50))
 
     bplot.tick_params(axis='y', labelsize=18)
     plt.ylabel("Number of copies per MITE family", fontsize=20)
     bplot.tick_params(axis='x', labelsize=18)
     plt.xlabel(None)
+    plt.ylim(0, 20)
     plt.show()
 
 
-boxplot(superfam_all_mites)
+# boxplot(superfam_all_mites)
+boxplot(superfam_de005)
