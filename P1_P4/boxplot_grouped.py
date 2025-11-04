6e1741e13e89cab245cfa5ae9a7b0cb3919055b3
Emilia Moranska <emilia.moranska@gmail.com>
2025-11-04 11:03:46 +0100

    Changes in displaying

--- P1_P4/boxplot_grouped.py
+++ P1_P4/boxplot_grouped.py
@@ -4,18 +4,32 @@ import matplotlib.pyplot as plt
 
 sns.set_theme(style="ticks", palette="deep")
 # mites_plants = pd.read_csv('../files/boxplots/P1_MITEs_in_genes_plants_boxplot_data.csv', sep='\t')
-mites_plants = pd.read_csv('../files/boxplots/P1_P4_all_MITEs_plants_to_boxplot.csv', sep='\t')
-
-# sns.boxplot(x="mite_group", y="Mite_sum", hue="mite_loc", data=mites_plants)
-# sns.despine(offset=10, trim=True)
+mites_plants = pd.read_csv('../files/boxplots/P1_P4_uc_plants_to_boxplot.csv', sep='\t')
 
 plt.figure(figsize=(14, 10))
-bplot = sns.boxplot(x="mite_loc", y="mite_sum", hue="F2_family", data=mites_plants)
+# sns.boxplot(x="mite_group", y="Mite_sum", hue="mite_loc", data=mites_plants)
+# sns.despine(offset=10, trim=True)
+bplot = sns.boxplot(x="mite_loc", y="mite_sum", data=mites_plants, color="white", fliersize=0)
+
+# Overlay scatter (or swarm) points colored by F2_family
+strplot = sns.stripplot(
+    x="mite_loc",
+    y="mite_sum",
+    data=mites_plants,
+    hue="F2_family",
+    dodge=False,        # ensures all points stay within one box
+    jitter=0.03,        # spread the points
+    palette="Set2",     # choose a nice color palette
+    alpha=0.8
+)
+
+# bplot = sns.boxplot(x="mite_loc", y="mite_sum", hue="F2_family", data=mites_plants)
 # sns.violinplot(x="mite_loc", y="mite_sum", hue="pop", data=mites_plants, split=True, inner="quart")
-plt.title("All MITEs", fontsize=35)
+
+plt.title("uc", fontsize=35)
 plt.xlabel(None)
 plt.ylabel("Number of MITEs", fontsize=28)
-bplot.tick_params(axis='y', labelsize=25)
-bplot.tick_params(axis='x', labelsize=28, rotation=15)
+strplot.tick_params(axis='y', labelsize=25)
+strplot.tick_params(axis='x', labelsize=28, rotation=15)
 plt.legend(fontsize=25)
 plt.show()
--- P1_P4/lineplot_density.py
+++ P1_P4/lineplot_density.py
@@ -3,7 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 sns.set_theme(style="ticks", palette="deep")
-mites_dens = pd.read_csv('../files/lineplot_density/EL10_mite_density_data.csv', sep='\t')
+mites_dens = pd.read_csv('../files/lineplot_density/P4_mite_density_data.csv', sep='\t')
 
 # dens = sns.lineplot(data=mites_dens, x="range", y="all_mites_dens")
 mites_dens_wide = mites_dens.pivot(index="range", columns="chr", values="all_mites_dens")
@@ -12,9 +12,9 @@ print(mites_dens_wide.to_string(max_rows=10))
 plt.figure(figsize=(20, 10))
 
 dens_wide = sns.lineplot(data=mites_dens_wide)
-plt.title("EL10_1.0", fontsize=35)
+plt.title("P4", fontsize=35)
 plt.xlabel("Chromosome length [Mb]", fontsize=23)
-plt.ylabel("MITE density [per 10 kb]", fontsize=23)
+plt.ylabel("MITE density [per 100 kb]", fontsize=23)
 dens_wide.tick_params(axis='y', labelsize=20)
 dens_wide.tick_params(axis='x', labelsize=20)
 plt.legend(fontsize=14, loc='lower right')
